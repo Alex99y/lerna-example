4ff95191a1d7e62f7b5a5b2160e5ab097aa0baaf
Alexander <alexyammine94@gmail.com>
2021-11-14 16:13:44 -0400

    Update bar returns description

--- packages/bar_opp/index.js
+++ packages/bar_opp/index.js
@@ -3,7 +3,7 @@ const crypto = require("crypto")
 /**
  * 
  * @param {string} message 
- * @returns {string}
+ * @returns {string} Return sha256 hash
  */
 module.exports = (message) => {
     return crypto.createHash("SHA256").update(Buffer.from(message, "utf-8")).digest("hex")
